1e878c8059bc86782a98c6899045e1982fc4266e
jacoposergio <djago32@gmail.com>
2022-11-28 17:08:31 +0100

    done

--- Controllers/GuestController.cs
+++ Controllers/GuestController.cs
@@ -6,28 +6,12 @@ namespace la_mia_pizzeria_static.Controllers
 {
     public class GuestController : Controller
     {
-        private readonly ILogger<GuestController> _logger;
-
-        public GuestController(ILogger<GuestController> logger)
-        {
-            _logger = logger;
-        }
-
+        //si occupa soltanto di presentarci le navigazioni
         public IActionResult Index()
         {
             ViewData["title"] = "Home";
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
